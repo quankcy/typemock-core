dd5d1256303f242acecf6bf64255f1d993be08c5
Quankcy <53514905+quankcy@users.noreply.github.com>
2019-07-31 13:16:05 +0200

    #1 - create core functionality

--- src/page-on-request.ts
+++ src/page-on-request.ts
@@ -1,10 +1,14 @@
 import {Page} from "puppeteer";
 import {Interceptors} from "./interceptor-builder";
+import {Interceptor} from "./interceptor";
 
 async function pageOnRequest(page: Page, interceptors: Interceptors) {
     await page.setRequestInterception(true);
 
     page.on('request', request => {
-        interceptors.getFirstByRequest(request)
+        const interceptor: Interceptor = interceptors.getFirstByRequest(request);
+
+        if(!interceptor)
+            request.continue();
     })
 }
